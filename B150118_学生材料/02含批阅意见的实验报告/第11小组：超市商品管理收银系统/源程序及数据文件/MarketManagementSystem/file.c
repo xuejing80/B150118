dcd950bafabdfc7c0bd7d1d1471e717471c02b28
NUPTSAST <ctygood@gmail.com>
2016-08-31 09:51:56 +0800

    信息后端完成

--- B150118_学生材料/02含批阅意见的实验报告/第11小组：超市商品管理收银系统/源程序及数据文件/MarketManagementSystem/file.c
+++ B150118_学生材料/02含批阅意见的实验报告/第11小组：超市商品管理收银系统/源程序及数据文件/MarketManagementSystem/file.c
@@ -22,23 +22,57 @@ struct goods readcode(char code[]){
     }
     return read;
 }
+//显示现有的全部商品
+void showgoods(){
+    goods read;
+    FILE *fp;
+    if(fp = fopen(".\\1.txt","r")){
+            printf("   条码           名称       售价      成本     存量   \n");
+        while(!feof(fp))
+        {
+
+            fscanf(fp,"%s %s %lf %lf %d",read.code,read.name,&read.price,&read.chengben,&read.num);
+            printf("%10s   %10s %10.2f %10.2f   %10d\n",read.code,read.name,read.price,read.chengben,read.num);
+        }
+    }else{
+        printf("文件加载错误，请检查数据库文件");
+    }
+
+}
+//添加新的商品
+int addgoods(char code[],char name[],double price,double chengben,int num){
+        FILE *fp;
+         if(fp = fopen(".\\1.txt","a+")){
+            fprintf(fp,"%s %s %lf %lf %d",code,name,price,chengben,num);
+            fclose(fp);
+            return 1;
+         }else{
+        printf("文件加载错误，请检查数据库文件");
+        return 0;
+         }
+}
+
+
+
+
+
+
+
 //写入条码信息，创建订单
 long neworder(){
     int id;
+    goods read;
     srand( (unsigned)time( NULL ) );
     id = rand()%(9999999-1000000+1)+1000000; //随机产生订单号
+
+}
+
+struct goods * readorder(char orderid[]){
     FILE *fp;
-    if(fp = fopen(".\\2.txt","r")){
-    while(!feof(fp)){
-        fscanf(fp,"%s %s %lf %lf %d",read.code,read.name,&read.price,&read.chengben,&read.num);
-        if(!strcmp(code,read.code))
-            {
-                fclose(fp);
-                break;
-            }
-        }
-    }
-    else{
+    if(fp = fopen(".\\2.txt","r"))
+    {
+
+    }else{
         printf("文件加载错误，请检查数据库文件");
     }
-}
+};
--- B150118_学生材料/02含批阅意见的实验报告/第11小组：超市商品管理收银系统/源程序及数据文件/MarketManagementSystem/main.c
+++ B150118_学生材料/02含批阅意见的实验报告/第11小组：超市商品管理收银系统/源程序及数据文件/MarketManagementSystem/main.c
@@ -6,5 +6,13 @@ int main()
 {
   //  login();
    // return 0;
-neworder();
+char code[20] = "987444";
+char name[20] = "test";
+double price = 3.2;
+double chengben = 1.0;
+int num = 102;
+   addgoods(code,name,price,chengben,num);
+   getchar();
+      getchar();
+showgoods();
 }
