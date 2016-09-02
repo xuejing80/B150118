34f5ae36d7bc00d9ec5e8d9f05191bc101cbde35
Baymax1830 <614082964@qq.com>
2016-09-02 10:35:11 +0800

    系统界面美化1

--- B150118_学生材料/02含批阅意见的实验报告/第11小组：超市商品管理收银系统/源程序及数据文件/MarketManagementSystem/welcome.c
+++ B150118_学生材料/02含批阅意见的实验报告/第11小组：超市商品管理收银系统/源程序及数据文件/MarketManagementSystem/welcome.c
@@ -51,14 +51,15 @@ void adminabc()
 //登录函数
 int login()
 {
-    printf("欢迎进入超市商品进销存系统\n");
-    printf("请登录！\n");
-    printf("请输入用户名：");
+    printf("\t\t\t***************************\n");
+    printf("\t\t\t欢迎登录超市商品进销存系统\n");
+    printf("\t\t\t***************************\n");
+    printf("\t\t\t请输入用户名：");
     char user_name[200];
     char user_password[200];
     int c=0,i=0;
     scanf("%s",&user_name);
-    printf("请输入密码：");
+    printf("\t\t\t请输入密码：");
     while ((c=getch()) != '\r') //使密码输入的时候显示*号
     {
         user_password[i++] = c;
@@ -72,7 +73,7 @@ int login()
     if(!strcmp(user_name,admin)&& !strcmp(user_password,adminpassword))
     {
         system("cls");
-        printf("\n管理员你好\n");
+        printf("\n\t\t\t\t管理员你好\n");
         adminabc();
     }
     else
@@ -80,13 +81,14 @@ int login()
         if(!strcmp(user_name,check)&& !strcmp(user_password,checkpassword))
         {
             system("cls");
-            printf("\n收银员你好\n");
+            printf("\t\t\t************************\n");
+            printf("\t\t\t\t收银员你好\n");
             checkabc(0,0.0,0.0);
         }
         else
         {
 
-            printf("\n用户名或密码错误,请按回车重新登录\n");
+            printf("\n\t\t\t用户名或密码错误,请按回车重新登录\n");
             getchar();
             getchar();
             system("cls");
@@ -98,15 +100,16 @@ int login()
 //收银员函数
 void checkabc(int id,double total,double profit){
  int command;
-    printf("欢迎进入收银员系统\n");
-    printf("1、收银\n2、本日收银报表\n");
+    printf("\t\t\t    欢迎进入收银员系统\n");
+    printf("\t\t\t************************\n\n\n");
+    printf("\t\t        1收银\n\t\t        2本日收银报表\n\n\n");
     do                  //检测是否输入正确的指令如果不是则重新输入
     {
-        printf("请输入命令：");
+        printf("\t\t\t\t请输入命令：");
         scanf("%d",&command);
          printf("\n");
         if(command!=1&&command!=2)
-            printf("输入错误\n再次");
+            printf("\t\t\t\t输入错误\n再次");
 
     }
     while(command!=1&&command!=2);
@@ -118,8 +121,9 @@ void checkabc(int id,double total,double profit){
     else
     {
         system("cls");
-        printf("欢迎查看本日营销报表\n本次订单号：%d总价格：%.2f%利润：%.2f\n",id,total,profit);
-         printf("按任意键返回上一层");
+        printf("\t\t\t\t**********************\n");
+        printf("\t\t\t\t 欢迎查看本日营销报表\n\t\t\t\t**********************\n本次订单号：%d\n 总价格：%.2f%\n  利润：%.2f\n",id,total,profit);
+         printf("\n\n\n按任意键返回上一层");
          getchar();
          getchar();
          system("cls");
@@ -147,7 +151,7 @@ void inputgoods()
         }
         scanf("%s%s%s%s",name,price,chengben,num);
         if(!checknum(num) && !checknum(code) && !checknum(chengben) && !checknum(price))
-            printf("\n   条码:%s\n   名称:%s\n   价格:%s\n   成本:%s\n",code,name,price,chengben);
+            printf("\n   条码:%s\n   名称:%s\n 价格:%s\n   成本:%s\n",code,name,price,chengben);
         else{
             printf("您的输入有误，请重新输入。\n");
              s = 1;
@@ -178,7 +182,8 @@ void checkcode()
     int i=0;
     double total=0.0;
     order profit;
-    printf("欢迎进入收银系统\n请输入商品条形码(输入end结束)：\n");
+     printf("\t\t\t        *****************\n");
+   printf("\t\t\t\t欢迎进入收银系统\n\t\t\t        *****************\n请输入商品条形码(输入end结束):");
     do
     {
         scanf("%s",code);
@@ -198,11 +203,11 @@ void checkcode()
                 profit.profit = profit.profit + good[i-1].price-good[i-1].chengben;
                 profit.total = total;
                sale(good[i-1].code); //库存减一
-                printf("商品名称：%10s 价格：%10.2f元 存量:%d \n总价:%10.2f元\n",good[i-1].name,good[i-1].price,good[i-1].num-1,total);
+                printf("\n\n\t\t\t\t商品名称：%s \n\t\t\t\t 价格：%2f元 \n\t\t\t\t 存量:%d \n\t\t\t\t 总价:%2f元\n",good[i-1].name,good[i-1].price,good[i-1].num-1,total);
             }
             else
             {
-                printf("本商品已售罄，请重新输入");
+                printf("\t\t\t\t本商品已售罄，请重新输入");
                 continue;
             }
         }
